e0589160114e20d9d038fc7671ce5f7697c9ecf9
Helamã VPC <helamacorrea@gmail.com>
2025-06-19 02:54:41 -0300

    home page desktop

---
+++ project/scripts/home.js
@@ -0,0 +1,11 @@
+const mapCta = document.querySelector('#mapCta');
+const partCta = document.querySelector('#partCta');
+
+
+mapCta.addEventListener('click', () => {
+    window.location.href = 'chat.html';
+});
+
+partCta.addEventListener('click', () => {
+    window.location.href = 'partners.html';
+});
